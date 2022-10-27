290049543bb29f5b127e128c02a0f2059c64bff7
Shubham Parihar <shubhamparihar391@gmail.com>
2022-10-27 09:33:25 +0530

    style(HomePage): remove red bg and center the content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 export default function HomePage() {
 	return (
 		<>
-			<main className='flex justify-center bg-red-500 py-6'>
-				<p className='text-lg font-thin text-white sm:text-2xl md:text-4xl'>
-					🔋 Batteries included Next.js project template
-				</p>
+			<main className='grid min-h-screen grid-cols-1 place-items-center'>
+				<p className='text-lg font-thin sm:text-2xl md:text-4xl'>🔋 Batteries included Next.js project template</p>
 			</main>
 		</>
 	);
